cc0f4a2d90a22166fd09c6aefb6b272b7e4c7bd8
Yahya Sohail <captaincyber@localhost.localdomain>
2018-04-16 21:18:06 -0500

    Created script to clean data from twitter

---
+++ cleanUp.py
@@ -0,0 +1,23 @@
+import sqlite3
+import re
+
+cleanedTweets = []
+
+conn = sqlite3.connect('twitter-data-collector/database.sqlite')
+cur = conn.cursor()
+
+hashtagRe = re.compile('#[a-zA-Z]+')
+mentionRe = re.compile('@[a-zA-Z\d]+')
+twitterLinkRe = re.compile('https://t.co/[a-zA-Z\d]+')
+
+tweets = cur.execute('SELECT * FROM tweets')
+for tweet in tweets:
+    cleanedTweet = (hashtagRe.sub('', mentionRe.sub('', twitterLinkRe.sub('', tweet[2]))))
+    cleanedTweets.append((cleanedTweet, tweet[0]))
+
+cur.close()
+
+cur = conn.cursor()
+cur.executemany('UPDATE tweets SET body = ? WHERE id = ?', cleanedTweets)
+cur.close()
+conn.commit()
