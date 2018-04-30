ac179392bf97fb6553e6cc7709bb9342dac627a5
CaptainCyber <captaincyber@captaincybertech.com>
2018-04-30 15:17:39 -0500

    Move netowrk model into it's own file.

--- useNetwork.py
+++ useNetwork.py
@@ -1,48 +1,10 @@
 import sys
 import numpy as np
 import tensorflow as tf
-import sqlite3
-conn = sqlite3.connect('twitter-data-collector/database.sqlite')
-
-NNodesHL1 = 500
-NNodesHL2 = 500
-NNodesHL3 = 500
-
-c = conn.cursor()
-hashtags = c.execute('SELECT * FROM hashtags').fetchall()
-c.close()
-
-n_classes = len(hashtags)
+from network import neural_network_model
 
 x = tf.placeholder('float', [None, 280])
 y = tf.placeholder('float')
-
-
-def neural_network_model(data):
-    HL1 = {'weights': tf.Variable(tf.random_normal([280, NNodesHL1])),
-           'biases': tf.Variable(tf.random_normal([NNodesHL1]))}
-
-    HL2 = {'weights': tf.Variable(tf.random_normal([NNodesHL1, NNodesHL2])),
-           'biases': tf.Variable(tf.random_normal([NNodesHL2]))}
-
-    HL3 = {'weights': tf.Variable(tf.random_normal([NNodesHL2, NNodesHL3])),
-           'biases': tf.Variable(tf.random_normal([NNodesHL3]))}
-
-    OL = {'weights': tf.Variable(tf.random_normal([NNodesHL3, n_classes])),
-          'biases': tf.Variable(tf.random_normal([n_classes])), }
-
-    l1 = tf.add(tf.matmul(data, HL1['weights']), HL1['biases'])
-    l1 = tf.nn.relu(l1)
-
-    l2 = tf.add(tf.matmul(l1, HL2['weights']), HL2['biases'])
-    l2 = tf.nn.relu(l2)
-
-    l3 = tf.add(tf.matmul(l2, HL3['weights']), HL3['biases'])
-    l3 = tf.nn.relu(l3)
-
-    output = tf.add(tf.matmul(l3, OL['weights']), OL['biases'])
-
-    return output
 
 
 def use_neural_network(input):
@@ -57,7 +19,7 @@ def use_neural_network(input):
         sess.run(tf.initialize_all_variables())
         print input
         saver.restore(sess, './model.ckpt')
-        return sess.run(tf.argmax(prediction.eval(feed_dict={x: input}), 1)) + 1
+        output = prediction.eval(feed_dict={x: input})
 
 
-print use_neural_network('What happened?')
+print use_neural_network(sys.argv[1])
